1664d2764cd0c45822dbd66b8a85c337834418c9
xia423456 <37393876+xia423456@users.noreply.github.com>
2022-04-08 11:00:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,8 +9,8 @@
 #=============================================================
 
 # fw876/helloworld
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i ' s/^#\(.*small\)/\1/ ' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
